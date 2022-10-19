c6d5b3dbef7283892d031063af0f59e191d8e3b2
Daniel Momoh <daniel.momoh@stu.ocsb.ca>
2022-10-19 17:33:29 +0000

    completed Assign3.cpp

--- Assign3.cpp
+++ Assign3.cpp
@@ -6,11 +6,14 @@
 
 
 int main() {
+// Declaration
     char letter;
     int ascii;
+// Inputs
     std::cout << "Enter a Character: ";
     std::cin >> letter;
     ascii = letter;
+// process
     if (ascii >= 65 && ascii <= 90)
         std::cout << "It is an Uppercase Alphabet";
     else if (ascii >= 97 && ascii <= 122)
